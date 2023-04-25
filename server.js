7d9e74a1a431c010249aea029e511a7f7aac34fb
mikzanco <mikyzancolo97@gmial.com>
2023-04-25 19:04:12 +0200

    erro 404 when call a user that doesn't exist

--- server.js
+++ server.js
@@ -3,12 +3,46 @@ const fastify = require ('fastify')({
     logger:true
 })
 
+// importiamo gli utenti 
+const users = require('./data.json')
+
 // creaiamo un'APIREST semplice
 fastify.get('/', (req, reply) => {
     reply.send({
         hello: 'world'
     })
 })
+
+// importimao i nostri dati e li esponiamo tutti quanti
+fastify.get('/users', (req, reply) => {
+    // limit e offset li usiamo per mettere dei paletti. Con req.query andiamo a vedere la query string 
+    const q = req.query;
+    const limit = parseInt(q.limit) || 10
+    const offset = parseInt(q.limit) || 0
+    // prendiamo i primi x utenti
+    const data = users.slice(offset, limit + offset)
+
+    reply.send({
+        users: data
+    })
+})
+
+// se vogliamo prendere un utente specifico e quindi in base all'id:
+fastify.get('/users/:id', (req, reply) => {
+    const id = req.params.id
+    const user = users.find((user) => user.id === parseInt(id))
+
+    if(!user) {
+        reply.send({
+            error:404
+        })
+    }
+
+    reply.send({
+        user
+    })
+})
+
 
 // avviamo il server
 fastify.listen({port: 3000}, (err, address) => {
